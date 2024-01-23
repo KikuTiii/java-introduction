56c8e4460cc3059ad71579a1706dbcbde5953159
KikuTi <matheus.ksousa@sptech.school>
2024-01-23 11:13:24 -0300

    Studying the basics of Java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L; // L no final indica que é um valor long
+
+        // Tipos Ponto Flutuante
+        float floatVar = 3.14f; // f no final indica que é um valor float
+        double doubleVar = 2.71828;
+
+        // Tipo Caractere
+        char charVar = 'A';
+
+        // Tipo Booleano
+        boolean booleanVar = true;
+    }
+}
