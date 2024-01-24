d4636f7822f2c710d3a94ca721a758d4ddb07793
kikuti <matheus.ksousa@sptech.school>
2024-01-24 12:38:44 -0300

    first exercises

---
+++ src/ex02.java
@@ -0,0 +1,12 @@
+//Desenvolva um programa para ler os lados de um retângulo e calcular a área deste retângulo,
+//utilizando a fórmula: ÁREA=LADO1*LADO2.
+public class ex02 {
+
+    public static void main(String[] args){
+        int lado = 7;
+        int altura = 10;
+        int area = lado * altura;
+
+        System.out.println(area);
+    }
+}
