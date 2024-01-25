94f40d3b9d024536a62ac6ed39fbe181e4448a0e
kikuti <matheus.ksousa@sptech.school>
2024-01-25 12:32:46 -0300

    first exercises

---
+++ src/ex03.java
@@ -0,0 +1,12 @@
+//Desenvolva um programa para ler uma temperatura em graus Centígrados e apresentá-la
+//convertida em graus Fahrenheit. A fórmula de conversão é: F=(9*C+160)/5, onde F é a temperatura
+//em Fahrenheit e C é a temperatura em Centígrados.
+
+public class ex03 {
+    public static void main(String[] args){
+            int c = 35;
+            int f = (9*c + 160)/ 5;
+
+            System.out.println(f);
+    }
+}
