a9856a80e7bb5e4373eb4849076023498d6c974d
kikuti <matheus.ksousa@sptech.school>
2024-02-06 12:08:45 -0300

    exercises in Java

---
+++ src/ex16.java
@@ -0,0 +1,56 @@
+//Desenvolva um programa para calcular o reajuste salarial dos funcionários de um
+//empresa de desenvolvimento de softwares. O programa deverá solicitar a leitura do código
+//e o salário atual do funcionário e realizar o reajuste salarial de acordo com a tabela abaixo.
+//O programa deverá apresentar o salário antigo, o novo salário, o valor do reajuste e a
+//função do funcionário.
+//Código Função Percentual
+//        1 Operador 5%
+//        2 Programador 10%
+//        3 Analista 15%
+
+import java.util.Scanner;
+public class ex16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o salário atual: ");
+        double salarioAtual = scanner.nextDouble();
+
+        System.out.println("Insira o código de trabalho: ");
+        int codigo = scanner.nextInt();
+
+        if (codigo >= 1 && codigo <= 3) {
+            double percentualReajuste = 0;
+
+            if (codigo == 1) {
+                percentualReajuste = 0.05;
+            } else if (codigo == 2) {
+                percentualReajuste = 0.10;
+            } else {
+                percentualReajuste = 0.15;
+            }
+
+            double valorReajuste = salarioAtual * percentualReajuste;
+            double salarioNovo = salarioAtual + valorReajuste;
+
+            String funcao = "";
+            if (codigo == 1) {
+                funcao = "Operador";
+            } else if (codigo == 2) {
+                funcao = "Programador";
+            } else {
+                funcao = "Analista";
+            }
+
+            System.out.println("\nResultados:");
+            System.out.println("Salário antigo: R$ " + salarioAtual);
+            System.out.println("Novo salário: R$ " + salarioNovo);
+            System.out.println("Valor do reajuste: R$ " + valorReajuste);
+            System.out.println("Função do funcionário: " + funcao);
+        } else {
+            System.out.println("Código de função inválido.");
+        }
+
+        scanner.close();
+    }
+}
